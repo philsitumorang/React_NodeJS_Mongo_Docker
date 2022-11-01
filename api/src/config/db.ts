e6642c596cc7ab5f88a9510480df6821a75772ea
Phil Situmorang <philmsk@gmail.com>
2022-11-01 20:54:19 +0500

    remove mogno/data

---
+++ api/src/config/db.ts
@@ -0,0 +1,18 @@
+import * as mongoose from 'mongoose';
+
+async function connectToDB() {
+  try {
+    console.log('Connecting to DB...');
+    await mongoose.connect(`mongodb://${process.env.MONGODB_APPLICATION_USER}:${process.env.MONGODB_APPLICATION_PASS}@${process.env.MONGODB_IP}:${process.env.MONGODB_PORT}/${process.env.MONGODB_APPLICATION_DATABASE}`);
+    console.log('Connected successfully!');
+  } catch (err) {
+    setTimeout(() => {
+      console.log('Reconnect...');
+      connectToDB()
+    }, 2000)
+  }
+}
+
+export async function connect() {
+  await connectToDB();
+};
