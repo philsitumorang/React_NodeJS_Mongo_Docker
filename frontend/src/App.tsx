34f0bdd46834ed66f5508ac1d24efa3ea3cfa89f
Phil Situmorang <philmsk@gmail.com>
2025-04-07 17:10:36 +0100

    chore: add moonfly theme

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,22 @@
 import { useEffect, useState } from "react";
-import { useFormik } from 'formik';
+import { useFormik } from "formik";
 import { nanoid } from "@reduxjs/toolkit";
-import dayjs, { Dayjs } from 'dayjs';
+import dayjs, { Dayjs } from "dayjs";
 import { Box, Container, Typography, Snackbar } from "@mui/material";
-import Profile from './components/Profile/Profile';
-import ProfileCreate from './components/ProfileCreate/ProfileCreate';
+import Profile from "./components/Profile/Profile";
+import ProfileCreate from "./components/ProfileCreate/ProfileCreate";
 import Form from "./components/Form/Form";
-import { deleteUser, fetchUsers, IUser, selectAllUsers, selectCurrentUser, setCurrent, updateUser } from './api/slice';
-import { useAppSelector, useAppDispatch } from './hooks';
 import {
-  LightFontWeight, ProfileWrapper
-} from "./Style";
+  deleteUser,
+  fetchUsers,
+  IUser,
+  selectAllUsers,
+  selectCurrentUser,
+  setCurrent,
+  updateUser,
+} from "./api/slice";
+import { useAppSelector, useAppDispatch } from "./hooks";
+import { LightFontWeight, ProfileWrapper } from "./Style";
 import { initialValues, validation } from "./api/api";
 
 function App() {
@@ -18,12 +24,14 @@ function App() {
   const users = useAppSelector(selectAllUsers);
   const currentUser = useAppSelector(selectCurrentUser);
   const [open, setOpen] = useState(false);
-  const [pickerData, setPickerData] = useState<Dayjs | null>(dayjs('2022-04-07'));
+  const [pickerData, setPickerData] = useState<Dayjs | null>(
+    dayjs("2022-04-07"),
+  );
   const [openNotification, setOpenNotification] = useState("");
   const handleOpen = (user: IUser) => {
     dispatch(setCurrent(user));
     setOpen(true);
-  }
+  };
   const handleClose = () => setOpen(false);
 
   useEffect(() => {
@@ -32,25 +40,25 @@ function App() {
 
   const onHandle = (e: any) => {
     formik.handleChange(e);
-    const _user: any = {...currentUser};
+    const _user: any = { ...currentUser };
     _user[e.target.name] = e.target.value;
     dispatch(setCurrent(_user));
-  }
+  };
 
   const onDelete = () => {
-    const _user: any = {...currentUser};
-    dispatch(deleteUser(_user))
+    const _user: any = { ...currentUser };
+    dispatch(deleteUser(_user));
     handleClose();
     setOpenNotification("Deleted successfully.");
     setTimeout(() => setOpenNotification(""), 3000);
-  }
+  };
 
   const formik = useFormik({
     initialValues,
     validationSchema: validation(),
     onSubmit: (value: any) => {
       value._id = currentUser?._id;
-      value.salary = +value.salary.replace(/\D/g, '');
+      value.salary = +value.salary.replace(/\D/g, "");
       if (value.salary === 1) {
         value.salary = currentUser?.salary;
       }
@@ -83,23 +91,28 @@ function App() {
           pickerData={pickerData}
         />
       ) : null}
-      
+
       <Typography variant="h4" mt={4} mb={4} sx={LightFontWeight}>
-        <Box 
+        <Box
           component="img"
           sx={{ height: 30, marginRight: 1 }}
-          src={process.env.PUBLIC_URL + '/logo.png'}
+          src={process.env.PUBLIC_URL + "/logo.png"}
         />
         / Employees
       </Typography>
 
       <Box sx={ProfileWrapper}>
         <>
-          {users.value.map(user => <Profile key={user._id || nanoid()} user={user} handleOpen={handleOpen}/>)}
-          <ProfileCreate  />
+          {users.value.map((user) => (
+            <Profile
+              key={user._id || nanoid()}
+              user={user}
+              handleOpen={handleOpen}
+            />
+          ))}
+          <ProfileCreate />
         </>
       </Box>
-
     </Container>
   );
 }
